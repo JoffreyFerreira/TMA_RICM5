c79092d1374e27a5c6a34c0b6b3b1d762dda8532
tozenda <thom.ozenda@gmail.com>
2019-01-16 16:17:55 +0100

    Encodeur ready to be tested

--- encodeur.c
+++ encodeur.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#include <string.h>
 #include "pixmap_io.c"
-
-int pow2(int n){
-    int res = 1;
-    for (int i = 0; i < n; ++i){
-        res*=2;
-    }
-    return res;
-}
 
 int main(int argc, char **argv){
     unsigned char *dataImg;
+    char *fileName;
     int imgWidth, imgHeight;
-
-    if( (argc > 2) && ((dataImg = load_pixmap(argv[1], &imgWidth, &imgHeight)) != NULL) && argv[2] != NULL){
+    int fileNameLength;
+    if( (argc > 3) && ((dataImg = load_pixmap(argv[1], &imgWidth, &imgHeight)) != NULL) && argv[2] != NULL && argv[3] != NULL){
         printf("IMG Width : %d\n",imgWidth);
         printf("IMG Height : %d\n",imgHeight);
-
+        fileName = argv[3];
+        fileNameLength = strlen(fileName);
         FILE *file;
         unsigned char *buffer;
-        unsigned long imgLen;
+        unsigned int imgLen;
 
         file = fopen(argv[2],"rb");
         if(!file){
@@ -42,9 +37,55 @@ int main(int argc, char **argv){
         fread(buffer,imgLen,sizeof(unsigned char),file);
         fclose(file);
 
-        printf("FileToHide size : %ld\n", imgLen);
+        printf("FileToHide size : %d\n", imgLen);
+        printf("Longueur chaine : %ld\n", strlen(fileName));
+
+        int i = 0;
+        int tmp;
+        //on met la taille de l'image
+        int tailleImg = imgLen;
+        for(;i<16;i++){
+            tmp = (tailleImg & 0xC0000000) >> 30;
+            dataImg[i] = (dataImg[i] & 0xFC) + tmp;
+            tailleImg = tailleImg << 2;
+        }
+
+        //on met le nom du fichier
+        int indiceParcoursNomFichier = 0;
+        unsigned char tmp2;
+        int indiceParcoursImgData = 16;
+        for(;indiceParcoursNomFichier < fileNameLength; indiceParcoursNomFichier++){
+            tmp2 = fileName[indiceParcoursNomFichier];
+            for(int j=0; j<4;j++){
+                dataImg[indiceParcoursImgData] = (dataImg[indiceParcoursImgData] & 0xFC) + (tmp2 >> 6);
+                indiceParcoursImgData++;
+                tmp2 = (tmp2 << 2);
+            }
+        }
+        for(int i=0;i<3;i++){
+            tmp2 = NULL;
+            for(int j=0; j<4;j++){
+                dataImg[indiceParcoursImgData] = (dataImg[indiceParcoursImgData] & 0xFC) + (tmp2 >> 6);
+                indiceParcoursImgData++;
+                tmp2 = (tmp2 << 2);
+            }
+            indiceParcoursNomFichier++;
+        }
+
+        //on encode l'image
+        indiceParcoursImgData = 144;
+        int indiceParcoursImgAEncoder = 0;
+        for(; indiceParcoursImgAEncoder < imgLen; indiceParcoursImgAEncoder++){
+            tmp2 = buffer[indiceParcoursImgAEncoder];
+            for(int i=0;i<4;i++){
+                dataImg[indiceParcoursImgData] = (dataImg[indiceParcoursImgData] & 0xFC) + (tmp2 >> 6);
+                indiceParcoursImgData++;
+                tmp2 = (tmp2 << 2);
+            }
+        }
 
 
+        store_pixmap("output.pgm",dataImg,imgWidth,imgHeight);
 
         return 1;
     }
